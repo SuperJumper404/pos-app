c56bd47d2e4850dd2f2eb5870cd88af65e7220fa
joker <ns.ayness@gmail.com>
2026-03-15 15:34:54 +0100

    Forms vue correction

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,13 @@ module.exports = {
       name: 'frontend_smart_eat_staging',
       script: 'serve',
       env: {
-        PM2_SERVE_PATH: './dist/',
-        PM2_SERVE_PORT: 8083,
+        PM2_SERVE_PATH: './dist',
+        PM2_SERVE_PORT: 3000,
         PM2_SERVE_SPA: 'true',
       },
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
     },
   ],
 }
