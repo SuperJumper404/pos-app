8560d18468465047e057a86f8af15b3c4f375c9f
joker <ns.ayness@gmail.com>
2026-06-05 20:57:19 +0200

    feat: refactor order detail component for improved layout and mobile responsiveness

--- pages/orders/detail/_id.vue
+++ pages/orders/detail/_id.vue
@@ -1,279 +1,120 @@
 <template>
   <v-container>
-    <div v-if="!isMobile">
-      <v-card
-        v-if="loadPage"
-        outlined
-        class="mt-5 overflow-y-auto"
-        style="height: 350px"
-      >
-        <Loading />
-      </v-card>
-      <v-card
-        v-else
-        outlined
-        class="mt-5 overflow-y-auto"
-        style="height: 400px"
-      >
-        <v-card-text v-if="orderPaymentStatus" class="pb-0">
-          Statut paiement :
-          <v-chip small dark :color="paymentStatusColor(orderPaymentStatus)">
-            {{ paymentStatusText(orderPaymentStatus) }}
-          </v-chip>
-        </v-card-text>
+    <v-card
+      v-if="loadPage"
+      outlined
+      class="mt-5 overflow-y-auto"
+      style="height: 350px"
+    >
+      <Loading />
+    </v-card>
+
+    <v-card v-else outlined class="mt-5 order-detail-panel">
+      <v-card-text v-if="orderPaymentStatus" class="pb-0">
+        Statut paiement :
+        <v-chip small dark :color="paymentStatusColor(orderPaymentStatus)">
+          {{ paymentStatusText(orderPaymentStatus) }}
+        </v-chip>
+      </v-card-text>
+
+      <div class="order-detail-list">
         <v-card
           v-for="(itm, i) in detailOrder"
           :key="i"
           outlined
-          class="mb-3 d-flex justify-space-between align-center pa-2"
+          class="order-detail-item mb-3 pa-2"
         >
-          <v-img
-            :src="productImageSrc(itm.image)"
-            class="order-detail-image"
-            :aspect-ratio="4 / 3"
-            height="96"
-            width="128"
-            max-width="128px"
-          ></v-img>
-          <!-- //TODO Faire en sorte que les colonnes soient bien alignées sur mobile -->
-          <v-card-text class="">
-            <v-row align="center" no-gutters>
-              <!-- Produit -->
-              <v-col cols="2" class="d-flex align-center justify-center">
-                <div class="text-center">
-                  <div
-                    class="text-center text-truncate"
-                    style="
-                      font-weight: bold;
-                      font-size: large;
-                      color: rgba(0, 0, 0, 0.8);
-                    "
-                  >
+          <div class="order-detail-content">
+            <v-img
+              :src="productImageSrc(itm.image)"
+              class="order-detail-image"
+              :aspect-ratio="4 / 3"
+              height="96"
+              width="128"
+              max-width="128px"
+            ></v-img>
+
+            <div class="order-detail-body">
+              <div class="order-detail-main">
+                <div class="order-detail-product">
+                  <div class="order-detail-product-name">
                     {{ itm.name }}
                   </div>
-                  <div
-                    class="text-center text-truncate"
-                    style="
-                      font-weight: bold;
-                      font-size: large;
-                      color: rgba(0, 0, 0, 0.8);
-                    "
-                  >
-                    {{ formatCurrency(itm.total) }}
+                  <div class="order-detail-product-price">
+                    {{ lineItemAmount(itm) }}
                   </div>
                 </div>
-              </v-col>
 
-              <!-- Customisations -->
-              <v-col cols="4" class="text-center">
-                <v-chip
-                  v-for="(customization, i) in itm.customizationList"
-                  :key="i"
-                  class="ma-1"
-                >
-                  {{ customization.name }}
-                </v-chip>
-              </v-col>
-
-              <!-- Numéro de commande -->
-              <v-col cols="3" class="text-center">
-                <div style="font-size: large; color: rgba(0, 0, 0, 0.8)">
-                  Numéro de commande
-                </div>
-                <h6
-                  class="text-center text-truncate"
-                  style="
-                    font-weight: bold;
-                    font-size: large;
-                    color: rgba(0, 0, 0, 0.8);
-                  "
-                >
-                  #{{ itm.ordernumber }}
-                </h6>
-              </v-col>
-
-              <!-- Client -->
-              <v-col cols="2" class="text-center">
-                <div style="font-size: large; color: rgba(0, 0, 0, 0.8)">
-                  Client
-                </div>
-                <h6
-                  class="text-center text-truncate"
-                  style="
-                    font-weight: bold;
-                    font-size: large;
-                    color: rgba(0, 0, 0, 0.8);
-                  "
-                >
-                  {{ itm.customer }}
-                </h6>
-              </v-col>
-
-              <!-- Quantité -->
-              <v-col cols="1" class="text-center">
-                <v-btn
-                  style="font-size: x-large"
-                  color="success"
-                  fab
-                  small
-                  dark
-                >
-                  {{ itm.qty }}
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-card>
-      <v-alert
-        v-if="orderNote"
-        class="my-4"
-        outlined
-        text
-        type="info"
-        transition="scroll-x-transition"
-        border="left"
-      >
-        Notes : {{ orderNote }}
-      </v-alert>
-      <v-card color="grey lighten-3" class="mt-5">
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" class="text-none" @click="$router.go(-1)"
-            >Retour <v-icon small right>mdi-arrow-left</v-icon></v-btn
-          >
-        </v-card-actions>
-      </v-card>
-    </div>
-    <div v-if="isMobile">
-      <v-card
-        v-if="loadPage"
-        outlined
-        class="mt-5 overflow-y-auto"
-        style="height: 350px"
-      >
-        <Loading />
-      </v-card>
-      <v-card
-        v-else
-        outlined
-        class="mt-5 overflow-y-auto"
-        style="height: 400px"
-      >
-        <v-card-text v-if="orderPaymentStatus" class="pb-0">
-          Statut paiement :
-          <v-chip small dark :color="paymentStatusColor(orderPaymentStatus)">
-            {{ paymentStatusText(orderPaymentStatus) }}
-          </v-chip>
-        </v-card-text>
-        <v-card
-          v-for="(itm, i) in detailOrder"
-          :key="i"
-          outlined
-          class="mb-3 d-flex flex-column"
-          rounded="7"
-        >
-          <!-- TOP ROW -->
-          <v-row class="align-center mx-2 mt-2 w-100" no-gutters>
-            <!-- LEFT: image + name/price -->
-            <v-col class="d-flex align-center min-w-0">
-              <v-avatar size="75" rounded tile class="mr-3">
-                <v-img
-                  class="order-detail-avatar rounded-lg"
-                  :src="productImageSrc(itm.image)"
-                />
-              </v-avatar>
-
-              <div class="min-w-0">
                 <div
-                  class="text-truncate font-weight-bold"
-                  style="font-size: large; color: rgba(0, 0, 0, 0.8)"
+                  v-if="itm.customizationList && itm.customizationList.length"
+                  class="order-detail-customizations"
                 >
-                  {{ itm.name }}
+                  <v-chip
+                    v-for="(customization, j) in itm.customizationList"
+                    :key="j"
+                    class="ma-1 order-detail-customization-chip"
+                  >
+                    {{ customization.name }}
+                  </v-chip>
                 </div>
-                <div class="font-weight-bold" style="color: rgba(0, 0, 0, 0.8)">
-                  {{ formatCurrency(itm.price) }}
+
+                <div class="order-detail-meta">
+                  <div class="order-detail-meta-block">
+                    <span class="order-detail-meta-label">
+                      Numero de commande
+                    </span>
+                    <strong class="order-detail-meta-value">
+                      #{{ itm.ordernumber }}
+                    </strong>
+                  </div>
+                  <div class="order-detail-meta-block">
+                    <span class="order-detail-meta-label">Client</span>
+                    <strong class="order-detail-meta-value">
+                      {{ itm.customer }}
+                    </strong>
+                  </div>
                 </div>
               </div>
-            </v-col>
+            </div>
 
-            <!-- RIGHT: customer + order + qty -->
-            <v-col
-              cols="auto"
-              class="d-flex align-center justify-center ml-auto"
-              style="flex-wrap: nowrap"
-            >
-              <div class="text-right mr-8">
-                <div
-                  class="text-truncate font-weight-bold"
-                  style="color: rgba(0, 0, 0, 0.8)"
-                >
-                  {{ itm.customer }}
-                </div>
-                <div
-                  class="text-truncate font-weight-bold"
-                  style="color: rgba(0, 0, 0, 0.8)"
-                >
-                  #{{ itm.ordernumber }}
-                </div>
-              </div>
-
-              <v-btn
-                style="font-size: x-large"
-                class="mr-2"
-                color="success"
-                fab
-                small
-                dark
-              >
-                {{ itm.qty }}
-              </v-btn>
-            </v-col>
-          </v-row>
-
-          <!-- CHIPS ROW -->
-          <v-row class="mx-2 mb-2" no-gutters>
-            <v-col class="pt-2">
-              <v-chip
-                v-for="(customization, j) in itm.customizationList"
-                :key="j"
-                class="ma-1"
-              >
-                {{ customization.name }}
-              </v-chip>
-            </v-col>
-          </v-row>
+            <v-btn class="order-detail-qty" color="success" fab small dark>
+              {{ itm.qty }}
+            </v-btn>
+          </div>
         </v-card>
-      </v-card>
-      <v-alert
-        v-if="orderNote"
-        class="my-4"
-        outlined
-        text
-        type="info"
-        transition="scroll-x-transition"
-        border="left"
-      >
-        Notes : {{ orderNote }}
-      </v-alert>
-      <v-card color="grey lighten-3" class="mt-5">
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" class="text-none" @click="$router.go(-1)"
-            >Retour <v-icon small right>mdi-arrow-left</v-icon></v-btn
-          >
-        </v-card-actions>
-      </v-card>
-    </div>
-    <!-- <pre type="json">{{ detailOrder }}</pre> -->
+      </div>
+    </v-card>
+
+    <v-alert
+      v-if="orderNote"
+      class="my-4"
+      outlined
+      text
+      type="info"
+      transition="scroll-x-transition"
+      border="left"
+    >
+      Notes : {{ orderNote }}
+    </v-alert>
+
+    <v-card color="grey lighten-3" class="mt-5">
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" class="text-none" @click="$router.go(-1)">
+          Retour <v-icon small right>mdi-arrow-left</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
+
 <script>
 import price from '@/helpers/price'
 const {
   getPaymentStatusText,
   getPaymentStatusColor,
 } = require('@/helpers/paymentStatus')
+
 export default {
   mixins: [price],
   layout() {
@@ -290,14 +131,11 @@ export default {
   },
 
   computed: {
-    isMobile() {
-      return this.$vuetify.breakpoint.smAndDown
-    },
     staticURL() {
       return this.$store.get('staticURL').replace(/\/+$/, '')
     },
     detailOrder() {
-      return this.$store.get('orders/detailOrder')
+      return this.$store.get('orders/detailOrder') || []
     },
     orderNote() {
       const remark = this.detailOrder[0] && this.detailOrder[0].remark
@@ -310,7 +148,6 @@ export default {
   async mounted() {
     this.loadPage = true
     const res = await this.$store.dispatch('orders/getDetailOrder', this.id)
-    console.log('res detail order ID ', this.detailOrder)
     if (res) {
       this.loadPage = false
     }
@@ -319,6 +156,13 @@ export default {
     productImageSrc(image) {
       const fileName = image || 'default.png'
       return `${this.staticURL}/api/v1/imgproducts/${fileName}`
+    },
+    lineItemAmount(item) {
+      const amount =
+        item.total !== undefined && item.total !== null
+          ? item.total
+          : item.price
+      return this.formatCurrency(amount)
     },
     paymentStatusText(item) {
       return getPaymentStatusText(item)
@@ -329,20 +173,189 @@ export default {
   },
 }
 </script>
+
 <style scoped>
-.order-detail-image,
-.order-detail-avatar {
-  flex: 0 0 auto;
+.order-detail-panel {
+  max-height: 70vh;
+  overflow-y: auto;
+}
+
+.order-detail-list {
+  padding: 8px 16px 16px;
+}
+
+.order-detail-item {
+  overflow: hidden;
+}
+
+.order-detail-content {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  min-width: 0;
+  width: 100%;
 }
 
 .order-detail-image {
+  border-radius: 4px;
+  flex: 0 0 128px;
   min-width: 128px;
   width: 128px;
 }
 
-.order-detail-image ::v-deep .v-image__image,
-.order-detail-avatar ::v-deep .v-image__image {
+.order-detail-body {
+  flex: 1 1 auto;
+  min-width: 0;
+}
+
+.order-detail-main {
+  display: grid;
+  grid-template-columns: minmax(140px, 1fr) minmax(140px, 1.5fr) minmax(
+      180px,
+      1fr
+    );
+  align-items: center;
+  gap: 16px;
+  min-width: 0;
+}
+
+.order-detail-product {
+  min-width: 0;
+}
+
+.order-detail-product-name,
+.order-detail-product-price,
+.order-detail-meta-value {
+  color: rgba(0, 0, 0, 0.8);
+  font-weight: bold;
+}
+
+.order-detail-product-name {
+  font-size: 18px;
+  line-height: 1.2;
+  overflow-wrap: anywhere;
+}
+
+.order-detail-product-price {
+  font-size: 17px;
+  margin-top: 4px;
+}
+
+.order-detail-customizations {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  min-width: 0;
+}
+
+.order-detail-customization-chip {
+  font-size: 15px;
+  min-height: 32px;
+}
+
+.order-detail-meta {
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  gap: 12px;
+}
+
+.order-detail-meta-block {
+  min-width: 0;
+  text-align: center;
+}
+
+.order-detail-meta-label {
+  color: rgba(0, 0, 0, 0.65);
+  display: block;
+  font-size: 13px;
+  line-height: 1.2;
+}
+
+.order-detail-meta-value {
+  display: block;
+  font-size: 17px;
+  line-height: 1.2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.order-detail-qty {
+  flex: 0 0 auto;
+}
+
+.order-detail-qty ::v-deep .v-btn__content {
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 1;
+}
+
+.order-detail-image ::v-deep .v-image__image {
   background-position: center;
   background-size: cover;
+}
+
+@media (max-width: 960px) {
+  .order-detail-content {
+    align-items: flex-start;
+    gap: 12px;
+  }
+
+  .order-detail-image {
+    flex-basis: 84px;
+    height: 84px !important;
+    min-width: 84px;
+    width: 84px;
+  }
+
+  .order-detail-main {
+    grid-template-columns: 1fr;
+    gap: 8px;
+  }
+
+  .order-detail-customizations {
+    justify-content: flex-start;
+  }
+
+  .order-detail-meta {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+@media (max-width: 600px) {
+  .order-detail-list {
+    padding: 8px;
+  }
+
+  .order-detail-content {
+    display: grid;
+    grid-template-columns: 76px minmax(0, 1fr) auto;
+    gap: 10px;
+  }
+
+  .order-detail-image {
+    flex: none;
+    height: 76px !important;
+    min-width: 76px;
+    width: 76px;
+  }
+
+  .order-detail-product-name {
+    font-size: 16px;
+  }
+
+  .order-detail-product-price,
+  .order-detail-meta-value {
+    font-size: 15px;
+  }
+
+  .order-detail-meta {
+    grid-template-columns: 1fr;
+    gap: 6px;
+  }
+
+  .order-detail-meta-block {
+    text-align: left;
+  }
 }
 </style>
