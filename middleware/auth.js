f5b2b1a9542ff99c79e54e93c68faff9c7e38707
joker <ns.ayness@gmail.com>
2026-08-01 15:11:02 +0200

    fix: restrict client access routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,9 +8,11 @@ export default function ({ store, redirect, route, router }) {
   const allowedPaths = ['/menus', '/ordersStatuses', '/login', '/cart']
   const allowedPathName = ['orders-detail-id']
 
+  const isClientAccess =
+    store.state.users.user.access === 2 || store.state.users.user.access === 3
+
   if (
-    store.state.users.user.access === 2 &&
-    store.state.users.user.access === 3 &&
+    isClientAccess &&
     !allowedPaths.includes(route.path) &&
     !allowedPathName.includes(route.name)
   ) {
